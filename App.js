d5243a476bdb8449c18b0f4ca24f4d5c4da9973d
Malaika Davis <malaika@Malaikacomputer.home>
2020-06-07 14:47:49 +0100

    Getting my code up and running

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import { fetch, decodeJpeg } from '@tensorflow/tfjs-react-native'
+// Load mobilenet.
+async function loadAndPredictImage(){
+
+
+const model = await mobilenet.load();
+
+// Get a reference to the bundled asset and convert it to a tensor
+const image = require('./assets/cutecatpic.jpeg');
+const imageAssetPath = Image.resolveAssetSource(image);
+const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
+const imageData = await response.arrayBuffer();
+
+const imageTensor = decodeJpeg(imageData);
+
+const prediction = await model.classify(imageTensor);
+return prediction
+
+// // Use prediction in app.
+// setState({
+//   prediction, // answer 
+  
+// });
+}
 
 export default function App() {
+  const prediction = loadAndPredictImage() 
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is Malaika's new app! (She is the best)</Text>
+      <Text>{prediction}</Text>
     </View>
   );
 }
@@ -17,3 +45,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
